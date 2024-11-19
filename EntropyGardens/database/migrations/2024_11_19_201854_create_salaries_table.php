75b1c996ff522edcfa5869b65e6a345cb0c6a8aa
nhelock <nhelock4003@stevenscollege.edu>
2024-11-19 15:21:03 -0500

    Created Salaries Table

---
+++ EntropyGardens/database/migrations/2024_11_19_201854_create_salaries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->integer('salaryID')->autoIncrement();
+            $table->integer('userID');
+            $table->integer('salary');
+
+            $table->primary('salaryID');
+            $table->foreign('userID')->references('userID')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salaries');
+    }
+};
