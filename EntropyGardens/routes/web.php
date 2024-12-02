aa25bcab0874cdec86b914d758ea98d971fc19f1
nhelock <nhelock4003@stevenscollege.edu>
2024-12-02 12:50:35 -0500

    Uncommented Role route

--- EntropyGardens/routes/web.php
+++ EntropyGardens/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/role', [PageController::class, 'Roles']);
+Route::get('/role', [PageController::class, 'Roles']);
 
 // Route::post('/role', [PageController::class, 'RolesTransfer']);
 
