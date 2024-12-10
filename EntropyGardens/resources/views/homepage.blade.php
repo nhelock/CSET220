2c0c78b9277ce8e2669637ddabf4b6870b7e1d0d
Luis Hernandez <149843869+LuisJ-H@users.noreply.github.com>
2024-12-10 14:49:59 -0500

    Homepage final changes
    Final changes for homepage

--- EntropyGardens/resources/views/homepage.blade.php
+++ EntropyGardens/resources/views/homepage.blade.php
@@ -22,44 +22,37 @@
     <title>Entropy Gardens</title>
   </head>
   <body>
-    <secion class="hero">
-  {{-- </head>
-  <body> --}}
-   
     <section class="hero">
-      <div class="container">
+      <div class="container" >
         <div class="heading">
           <span>Entropy Gardens Medical Center</span>
           <p>Where Everyday Brings New Hope -- Entropy Gardens</p>
         </div>
+        <div class="contact-container">
+          <div class="contact-info">
+            <p>
+              <i class="fas fa-map-marker-alt"></i>750 E King St <br />
+              Lancaster, PA 17601
+            </p>
+            <p>
+              <i class="fas fa-phone"></i>
+              <a href="tel:2232879000">+1 (717) 712-8381</a>
+            </p>
+          </div>
+          <div class="map">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3048.879919174703!2d-76.32073548429399!3d40.05812007940912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c624d65c13e70d%3A0x91fd342d5394903!2sPenn%20State%20Health%20Lancaster%20Medical%20Center!5e0!3m2!1sen!2sus!4v1700000000000!5m2!1sen!2sus"
+              width="400"
+              height="300"
+              style="border: 0"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
       </div>
     </secion>
-    </section>
-    <section class="info">
-      <div class="contact-container">
-        <div class="contact-info">
-          <p>
-            <i class="fas fa-map-marker-alt"></i>750 E King St <br />
-            Lancaster, PA 17601
-          </p>
-          <p>
-            <i class="fas fa-phone"></i>
-            <a href="tel:2232879000">+1 (717) 712-8381</a>
-          </p>
-        </div>
-        <div class="map">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3048.879919174703!2d-76.32073548429399!3d40.05812007940912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c624d65c13e70d%3A0x91fd342d5394903!2sPenn%20State%20Health%20Lancaster%20Medical%20Center!5e0!3m2!1sen!2sus!4v1700000000000!5m2!1sen!2sus"
-            width="400"
-            height="300"
-            style="border: 0"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
-      </div>
-    </section>
     <footer>
       <div class="footer-container">
         <div class="row">
