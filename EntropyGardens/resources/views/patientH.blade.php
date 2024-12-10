d8140f45e8914042f0b42381050d9675b038fa4b
nhelock <nhelock4003@stevenscollege.edu>
2024-12-10 08:07:51 -0500

    Implemented Patient Home Page styling

--- EntropyGardens/resources/views/patientH.blade.php
+++ EntropyGardens/resources/views/patientH.blade.php
@@ -3,22 +3,33 @@
 @section('title', 'Patient Home')
 
 @section('content')
-    <link rel=stylesheet href={{ asset('CSS/familyHome.css') }}>
-    <h1>UserID: {{ $userID }}</h1>
-    <h1>Name: {{ $name }}</h1>
-    <h1>Date: {{ $date }}</h1>
+    <link rel=stylesheet href={{ asset('CSS/patientOfDoctor.css') }}>
+    <h1 class=lesser-elements>UserID: {{ $userID }}</h1>
+    <h1 class=lesser-elements>Name: {{ $name }}</h1>
+    <h1 class=lesser-elements>Date: {{ $date }}</h1>
 
     <form action=/patient method=POST>
         @csrf
-        <label for=date>Search for Itinerary by Date: </label>
-        <input type=date name=date placeholder={{ $date }} value={{ $date }}>
-        <button type=submit>Search</button>
+        <table class=prescription>
+          <tr>
+            <th><label for=date>Search for Itinerary by Date: </label></th>
+          </tr>
+          <tr>
+            <th><input type=date name=date placeholder={{ $date }} value={{ $date }}></th>
+          </tr>
+          <tr>
+            <th><button type=submit class=prescription-button>Search</button></th>
+          </tr>
+        </table>
+        
+        
+        
     </form>
 
     <?php if(isset($data)){ ?>
 
         <div class="schedule-container">
-          <table>
+          <table class=prescription>
               <tr>
                 <th>Doctor's Name</th>
                 <th>Doctor's Appointment</th>
